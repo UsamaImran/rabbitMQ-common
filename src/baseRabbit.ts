547d26ef3b57d22c3ef249eed35e5c845238750c
Usama Imran <uusamaimran@gmail.com>
2026-05-09 22:22:07 +0500

    Revert "Add Vitest tests, config, and baseRabbit updates"
    This reverts commit 34624c70e395ee1ae6cdb3d2ce5a10531ea5ea57.

--- src/baseRabbit.ts
+++ src/baseRabbit.ts
@@ -11,22 +11,43 @@ export abstract class BaseRabbit {
   async init() {
     if (this.channel) return;
 
-    if (this.initPromise) return this.initPromise;
+    if (this.initPromise) {
+      await this.initPromise;
+      return;
+    }
 
-    this.initPromise = this._connect();
-    return this.initPromise;
+    this.initPromise = (async () => {
+      try {
+        this.connection = await amqplib.connect(this.url);
+
+        this.connection.on("close", () => {
+          console.error("[RabbitMQ] Connection closed");
+        });
+
+        this.connection.on("error", (err) => {
+          console.error("[RabbitMQ] Connection error:", err);
+        });
+
+        this.channel = await this.connection.createChannel();
+
+        console.log(
+          `[RabbitMQ] Connected to host: ${new URL(this.url).hostname}`,
+        );
+      } catch (error) {
+        console.error("[RabbitMQ] Connection error:", error);
+        throw error;
+      }
+    })();
+
+    await this.initPromise;
   }
 
-  private async _connect() {
-    this.connection = await amqplib.connect(this.url);
-    this.channel = await this.connection.createChannel();
+  async close() {
+    await this.channel?.close();
+    await this.connection?.close();
 
-    this.connection.on("close", () => {
-      console.error("[RabbitMQ] Connection closed");
-    });
-
-    this.connection.on("error", (err) => {
-      console.error("[RabbitMQ] Connection error:", err);
-    });
+    this.channel = undefined;
+    this.connection = undefined;
+    this.initPromise = undefined;
   }
 }
